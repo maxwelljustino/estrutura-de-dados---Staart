c02689884d6e48f199f8bb28afc951ab94ac8a24
maxwelljustino <maxwell-justino@hotmail.com>
2022-08-27 17:59:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-m8flue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-m8flue)
---
+++ index.js
@@ -0,0 +1,6 @@
+// run `node index.js` in the terminal
+
+const somar = (x = 0, y = 0) => {
+  return x+y;
+};
+console.log(somar(6,9));
