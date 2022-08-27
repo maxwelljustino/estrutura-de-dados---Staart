8434bb22f23ffd3d88349ab08bcae36a13441ece
maxwelljustino <maxwell-justino@hotmail.com>
2022-08-27 19:05:31 -0300

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 // run `node index.js` in the terminal
+/*
+const saldoMaxwell = 100
+const saldoMarina = 200
 
-const somar = (x = 0, y = 0) => {
-  return x+y;
-};
-console.log(somar(6,9));
+const total = saldoMaxwell + saldoMarina;
+console.log(total);
+*/
+
+// Vetores - Arrays
+/*
+const saldoContas = [100, 200, 500, 50, 1.9, 2.99, 300];
+
+const total = saldoContas.reduce ((acumulador, valor) => acumulador + valor);
+ console.log(total);
+
+ // Função 'reduce' - Realiza a soma de todos os itens do array
+ */
+
+ // Matriz de 3 dimensões
+/*
+ const bandeiras = [
+   ['Brasil', ['verde', 'branco', 'azul', 'amarela']],
+   ['Canadá', ['vermelho', 'branco']],
+   ['México', ['verde', 'branco', 'vermelho']],
+   ['EUA', ['azul', 'Branco', 'vermelho']],
+ ];
+ console.log(bandeiras[1][1][0]);
+ */
+  
+
+
