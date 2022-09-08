510ee450fbaa62f6f2012b541e4b263a4f4478de
maxwelljustino <93413365+maxwelljustino@users.noreply.github.com>
2022-09-07 22:05:54 -0300

    1 file modified

--- index.js
+++ index.js
@@ -28,5 +28,71 @@ const total = saldoContas.reduce ((acumulador, valor) => acumulador + valor);
  console.log(bandeiras[1][1][0]);
  */
   
+ /*
+ 
+let frutas = new Array('Abacaxi');
+
+frutas.push('Banana');
+frutas.push('Maçã');
+frutas.push('Morango');
+frutas.unshift('Tangerina');
+
+frutas.pop();
+frutas.shift();
+frutas.splice(2, 1, 'Fruta do Conde');
+console.table(frutas);
+
+const redesSociais = [
+  'Instagram',
+  'YouTube',
+  'Facebook',
+  'TikTok',
+  'Twitter',
+  'Linkedin',
+  'Pinterest',
+];
+
+
+const tiktok = redesSociais.indexOf('TikTok');
+console.log(tiktok);
+
+console.log (redesSociais.includes ('tumblr'));
+
+for (let i=0; i < redesSociais.length; i++) {
+  console.log(redesSociais[i]);
+}
+
+for (const i in redesSociais) {
+console.log(`${i}: ${redesSociais[i]}`);
+}
+
+for (const valor of redesSociais){
+  console.log(valor);
+}
+
+redesSociais.forEach((valor, indice, redes) => {
+  console.log(valor);
+  console.log(indice);
+  console.log(redes);
+})
+
+let i = 0
+while (i < redesSociais.length) {
+  console.log(redesSociais[i]);
+  i++;
+}
+
+
+let i = 0
+do {
+  console.log(redesSociais[i]);
+  i++;
+}
+  while (i < redesSociais.length);
+
+*/
+
+
+
 
 
